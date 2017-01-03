00fe16486be1c9e0afb996e7bc0e03bf46a34527
Jan Schulte <j.schulte@52north.org>
2017-01-03 13:52:17 +0100

    switch legacy http methods in nominatim service

--- dist/ui-leaflet.js
+++ dist/ui-leaflet.js
@@ -2894,9 +2894,9 @@ angular.module('ui-leaflet').factory('nominatimService', ["$q", "$http", "leafle
             var url = defaults.nominatim.server;
             var df = $q.defer();
 
-            $http.get(url, { params: { format: 'json', limit: 1, q: address } }).success(function (data) {
-                if (data.length > 0 && isDefined(data[0].boundingbox)) {
-                    df.resolve(data[0]);
+            $http.get(url, { params: { format: 'json', limit: 1, q: address } }).then(function (response) {
+                if (response.data.length > 0 && isDefined(response.data[0].boundingbox)) {
+                    df.resolve(response.data[0]);
                 } else {
                     df.reject('[Nominatim] Invalid address');
                 }
--- src/services/nominatim.js
+++ src/services/nominatim.js
@@ -7,9 +7,9 @@ angular.module('ui-leaflet').factory('nominatimService', function ($q, $http, le
             var url = defaults.nominatim.server;
             var df = $q.defer();
 
-            $http.get(url, { params: { format: 'json', limit: 1, q: address } }).success(function(data) {
-                if (data.length > 0 && isDefined(data[0].boundingbox)) {
-                    df.resolve(data[0]);
+            $http.get(url, { params: { format: 'json', limit: 1, q: address } }).then(function(response) {
+                if (response.data.length > 0 && isDefined(response.data[0].boundingbox)) {
+                    df.resolve(response.data[0]);
                 } else {
                     df.reject('[Nominatim] Invalid address');
                 }
